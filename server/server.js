1d6773fddf72522df45b0c762a8dee7f064704a2
abhinav-94 <abhinav.2k17@gmail.com>
2018-06-12 21:00:55 -0700

    new

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const path=require('path'); //no need to import already available in node
 // console.log(__dirname + '/../public');
+const http=require('http'); //built in module no need to install we can simply require
 const express=require('express');
+const socketIO=require('socket.io');
 var app=express(); //app variable for configuring our express application
+
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT||3000; //doing it for Heroku Deployment
 console.log(publicPath);
-
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+  socket.on('disconnect',()=>{
+    console.log("User was disconnected");
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });  //start the server at port 3000 and a little message onec the server is up
